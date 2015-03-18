5212d21b24135cea530107848a2dbec0a99e25ee
Attila SATAN <attila.satan@gmail.com>
2015-03-19 01:59:00 +0200

    Update tweetopia.js

--- tweetopia.js
+++ tweetopia.js
@@ -1,8 +1,8 @@
-var port = process.env.PORT;
+var port = process.env.PORT || 3000;
 
 var express = require("express");
 var app = express();
-app.use(express.logger());
+//app.use(express.logger());
 app.use(express.static(__dirname + '/public'));
 
 
@@ -10,8 +10,8 @@ app.use(express.static(__dirname + '/public'));
 
 var Twitter = require('mtwitter');
 var twitter = new Twitter({
-	consumer_key: process.env.TWITTER_CONSUMER_KEY,
-	consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+	consumer_key: process.env.TWITTER_CONSUMER_KEY || "burası consumer key" ,
+	consumer_secret: process.env.TWITTER_CONSUMER_SECRET || "secret'ı buraya yaz",
 	application_only: true
 });
 
@@ -23,7 +23,7 @@ app.get('/ws', function(request, response) {
 	var options = JSON.parse(JSON.stringify(request.query));
 
 	if (options.q == null) {
-		options.q = '%23WebGL';
+		options.q = '%23MeHow'; // buraya standart hashtagi yaz mesela %23Tangun gibi
 	}
 
 	options.callback = null;
@@ -32,11 +32,12 @@ app.get('/ws', function(request, response) {
 		'search/tweets',
 		options, 
 		function(err, item) {
-			console.log("ERROR: " + err)
-			console.log("ITEM: " + item);
+			if(err)
+				return console.log("ERROR: " + err)
+			//console.log(item);
 
 			if (item != null) {
-				console.log("cool");
+				//console.log("cool");
 
 				 var json = JSON.stringify(item);
 				// response.writeHead(200, {'content-type':'application/json'}); 
